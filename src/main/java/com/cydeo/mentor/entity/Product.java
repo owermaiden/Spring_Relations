9c6d858454438c5aa699b653bd5644c2691f837c
owermaiden <ower.maiden@cybertekschool.com>
2023-04-26 09:37:23 -0400

    step 2 Table Created

--- src/main/java/com/cydeo/mentor/entity/Product.java
+++ src/main/java/com/cydeo/mentor/entity/Product.java
@@ -1,11 +1,8 @@
 package com.cydeo.mentor.entity;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
 import lombok.Data;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
